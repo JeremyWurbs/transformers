35538ba36299ae27008f8f6a665a9c733505b399
Jeremy <jdwurbs@gmail.com>
2023-04-27 14:02:41 +0900

    Make oak classes available at package scope

---
+++ oak/__init__.py
@@ -0,0 +1,9 @@
+from oak.data import MNIST
+from oak.core.mlp import MLP
+from oak.core.embedding import Embedding
+from oak.core.attention import Attention
+from oak.core.mhsa import MultiHeadSelfAttention
+from oak.core.sab import SelfAttentionBlock
+from oak.core.vision_transformer import VisionTransformer
+from oak.utils import LightningModel
+from oak.models import ViTBase, ViTLarge, ViTHuge
