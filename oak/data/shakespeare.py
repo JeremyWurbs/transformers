26c96bea69cbe5855c3de64a33021ccb6bb31530
Jeremy <jdwurbs@gmail.com>
2023-05-24 11:13:16 +0900

    Add extra display out to Shakespeare training script

--- oak/data/shakespeare.py
+++ oak/data/shakespeare.py
@@ -8,17 +8,15 @@ from oak import ShakespeareTokenizer
 
 
 class ShakespeareDataset(Dataset):
-    def __init__(self, data, block_size=8, batch_and_mask=False):
+    def __init__(self, data, block_size=8):
         assert len(data) >= block_size
         self.data = data
         self.block_size = block_size
-        self.batch_and_mask = batch_and_mask
 
     def __len__(self):
         return len(self.data)-self.block_size
 
     def __getitem__(self, idx):
-        #idx = torch.randint(self.__len__(), (1,))
         x = torch.tensor(self.data[idx:idx + self.block_size])
         y = torch.tensor(self.data[idx + 1:idx + self.block_size + 1])
         return x, y
@@ -38,9 +36,9 @@ class ShakespeareDataModule(pl.LightningDataModule):
 
         m = int(train_percent * len(text))
         n = int(val_percent * len(text))
-        self.train_data = ShakespeareDataset(data=text[:m], block_size=block_size, batch_and_mask=True)
-        self.val_data = ShakespeareDataset(data=text[m:m+n], block_size=block_size, batch_and_mask=True)
-        self.test_data = ShakespeareDataset(data=text[m+n:], block_size=block_size, batch_and_mask=True)
+        self.train_data = ShakespeareDataset(data=text[:m], block_size=block_size)
+        self.val_data = ShakespeareDataset(data=text[m:m+n], block_size=block_size)
+        self.test_data = ShakespeareDataset(data=text[m+n:], block_size=block_size)
 
     @property
     def vocab_size(self):
--- scripts/shakespeare.py
+++ scripts/shakespeare.py
@@ -3,9 +3,9 @@ import torch
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers.tensorboard import TensorBoardLogger
 
-from oak import Shakespeare, NLPTransformer, LightningModel, ShakespeareTokenizer
+from oak import Shakespeare, GPT, LightningModel, Tokenizer, ShakespeareTokenizer
 
-#torch.set_float32_matmul_precision('medium')
+torch.set_float32_matmul_precision('medium')
 
 param = {
     'num_blocks': 6,
@@ -17,12 +17,13 @@ param = {
     'lr': 1e-3,
 }
 
-dm = Shakespeare(batch_size=param['batch_size'], block_size=param['seq_len'], num_workers=48, tokenizer=ShakespeareTokenizer())
-model = NLPTransformer(vocab_size=dm.vocab_size, **param)
+tokenizer = ShakespeareTokenizer()  # Use Tokenizer() for gpt2 tokens, or ShakespeareTokenizer() for char-level tokens
+dm = Shakespeare(batch_size=param['batch_size'], block_size=param['seq_len'], num_workers=48, tokenizer=tokenizer)
+model = GPT(vocab_size=dm.vocab_size, **param)
 
 gen_id = [0]
-def validation_hook():
-    generated_text = model.model.generate(torch.zeros((1, 1), dtype=torch.long).to('cuda:1'), max_new_tokens=250)
+def validation_hook(max_new_tokens=1000):
+    generated_text = model.model.generate(torch.zeros((1, 1), dtype=torch.long).to('cuda:1'), max_new_tokens=max_new_tokens)
     generated_text = dm.tokenizer.decode(generated_text[0, :].cpu().detach().numpy())[0][0]
     print(f"{generated_text}")
     with open('generated_text.txt', 'a') as f:
@@ -35,3 +36,5 @@ logger = TensorBoardLogger(save_dir=os.getcwd(), name=f"{os.path.join('experimen
 trainer = Trainer(max_epochs=1, logger=logger, val_check_interval=.05, devices=[1])
 trainer.fit(model, dm)
 trainer.test(model, dm)
+
+validation_hook(10000)
